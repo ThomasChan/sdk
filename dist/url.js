04e45e74c0db8a2c51af04a19ea77ae642ecbbf2
Marshall Chen <juniors.fei@gmail.com>
2015-11-18 18:32:06 +0800

    curly braces fix

--- dist/url.js
+++ dist/url.js
@@ -11,6 +11,6 @@ var _lodash2 = _interopRequireDefault(_lodash);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = function (route, locals) {
-  return _lodash2.default.template(route)(locals);
+  return _lodash2.default.template(route)({ locals: locals });
 };
 //# sourceMappingURL=url.js.map
